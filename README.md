139c91baf749988ad05dad40924588bcd46b4017
David Harrigan <dharrigan118@gmail.com>
2017-06-27 23:43:50 -0400

    update flags

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 `sup` is a command line utility that will show you outstanding TODO items in the specified path.
 If the path belongs to a git repository, the result can further be filtered by the author who made the commit, so you can quickly track down your outstanding work.
+
+## Usage
+
+## Install
+
+## TODO
+- author lookup
+- tests
+- ci
--- cmd/list.go
+++ cmd/list.go
@@ -33,20 +33,13 @@ func listRun(cmd *cobra.Command, args []string) {
 		root = args[0]
 	}
 	result := todo.Search(root)
+	// determine if this is a git directory
 	todo.PrintSearchResults(result)
 }
 
 func init() {
 	RootCmd.AddCommand(listCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
+	listCmd.Flags().BoolP("skip-author", "a", false, "Skip author lookup")
+	listCmd.Flags().StringP("email", "e", "", "Override author look up value (default git config --global user.email")
 }
